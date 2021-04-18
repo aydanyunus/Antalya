85451cff6b35b54237b37bace6a6e5eba1347250
aydanyunus <aydanyunus98@gmail.com>
2021-04-19 02:20:00 +0400

    blog-inner page design update

--- js/blog-inner.js
+++ js/blog-inner.js
@@ -1,17 +1,32 @@
 var galleryThumbs = new Swiper('.gallery-thumbs', {
-    spaceBetween: 15,
-    slidesPerView: 12,
-    freeMode: true,
-    watchSlidesVisibility: true,
-    watchSlidesProgress: true,
-  });
-  var galleryTop = new Swiper('.gallery-top', {
-    spaceBetween: 10,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+  spaceBetween: 15,
+  slidesPerView: 12,
+  freeMode: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+  breakpoints: {
+    300: {
+      slidesPerView: 4,
     },
-    thumbs: {
-      swiper: galleryThumbs
+    640: {
+      slidesPerView: 4,
+    },
+    768: {
+      slidesPerView: 4,
+    },
+    1024: {
+      slidesPerView: 12,
     }
-  });
+  }
+});
+var galleryTop = new Swiper('.gallery-top', {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  thumbs: {
+    swiper: galleryThumbs
+  },
+  
+});
