141aa1ebd37e26c1ed45c7c6101438e8a5541b4d
afsananadirova <afsanan@crocusoft.com>
2021-04-25 12:57:30 +0400

    changes-2504

--- js/portfolio-main.js
+++ js/portfolio-main.js
@@ -1,18 +1,61 @@
 $(document).ready(function () {
   $("#imageGallery").lightSlider({
     gallery: true,
-    fullScreen: true,
     item: 1,
     loop: true,
-    thumbItem: 8,
+    thumbItem: 9,
     slideMargin: 0,
-    autoplay: false,
-    gallery: true,
-    item: 1,
-    speed: 500,
-    pause: 5000,
-    auto: false,
-    loop: true,
+    enableDrag: false,
     currentPagerPosition: "left",
   });
+});
+$(".icon-fullscreen").click(function () {
+  $("#imageGallery").lightGallery({
+    fullScreen: true,
+  });
+});
+$(".full-home").click(function () {
+  $(".one").css("display", "none");
+  $(".two").css("display", "none");
+  $("#imageGallery").lightGallery({
+    fullScreen: true,
+  });
+});
+$(document).ready(function () {
+  $(".js-example-basic-single").select2();
+});
+
+jQuery(document).ready(function ($) {
+  //close popup
+  $(".cd-popup").on("click", function (event) {
+    if (
+      $(event.target).is(".cd-popup-close") ||
+      $(event.target).is(".cd-popup")
+    ) {
+      event.preventDefault();
+      $(this).removeClass("is-visible");
+    }
+  });
+  //close popup when clicking the esc keyboard button
+  $(document).keyup(function (event) {
+    if (event.which == "27") {
+      $(".cd-popup").removeClass("is-visible");
+    }
+  });
+});
+
+//open popup
+function openpopup(id) {
+  event.preventDefault();
+  $("#" + id + "").addClass("is-visible");
+}
+var vid = document.getElementById("home-video");
+function pauseVid() {
+  vid.pause();
+}
+$("#pop2-close").click(function () {
+  pauseVid();
+});
+$(".lg-close.lg-icon").click(function () {
+  $(".lg-start-zoom").css("display", "none");
 });
